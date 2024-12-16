b034f3d50c2f14f0a22370b5afe3213d49aca43b
Vijay11-SP <bluewarheart@gmail.com>
2024-12-16 09:34:27 +0530

    made some changes

--- app.js
+++ app.js
@@ -13,19 +13,20 @@ dotenv.config({
 const app = express()
 
 const PORT = process.env.PORT;
-// const corsOrigins = ['http://localhost:3000', 'http://localhost:5173']; // extract and split CORS origins from the environment variable
 
 // middleware configuration
 const corsOptions = {
     origin: process.env.CORS_ORIGIN, // frontend URI (ReactJS)
     credentials: true
 }
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://dapper-sundae-642fb7.netlify.app'); 
+  res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
   next();
 });
+
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
